9ebc3faa8a426a7872086a99eb1964033b5d3545
daichan132 <wakadai0319@gmail.com>
2023-11-14 15:22:59 +0900

    画面の上部にいるときだけchatを表示できるように修正

--- src/content/features/YTDLiveChat/hooks/useIsShow.ts
+++ src/content/features/YTDLiveChat/hooks/useIsShow.ts
@@ -1,16 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from 'react';
 import { useIsFullScreen } from './useIsFullScreen';
 
 export const useIsShow = (videoID: string) => {
   const isFullscreen = useIsFullScreen();
-  const [isShow, setIsShow] = useState<boolean>(false);
+  const [isLive, setIsLive] = useState<boolean>(false);
 
   useEffect(() => {
-    setIsShow(false);
+    setIsLive(false);
     if (!isFullscreen) return;
     const liveBadge = document.querySelector('.ytp-chrome-controls .ytp-live .ytp-live-badge');
     const live = liveBadge && !liveBadge.getAttribute('disabled');
-    if (live) setIsShow(true);
+    if (live) setIsLive(true);
   }, [isFullscreen, videoID]);
-  return isShow;
+
+  const [isTop, setIsTop] = useState<boolean>(false);
+  useEffect(() => {
+    setIsTop(true);
+    const ytdAppElement = document.querySelector('ytd-app');
+    if (!isFullscreen || !ytdAppElement) return;
+    if (ytdAppElement.hasAttribute('masthead-hidden')) {
+      setIsTop(true);
+    } else {
+      setIsTop(false);
+    }
+    const mastheadHidden = (mutations: any) => {
+      mutations.forEach((mutation: any) => {
+        if (mutation.target.hasAttribute('masthead-hidden')) {
+          setIsTop(true);
+        } else {
+          setIsTop(false);
+        }
+      });
+    };
+    const mutationObserver = new MutationObserver(mastheadHidden);
+    mutationObserver.observe(ytdAppElement, {
+      attributeFilter: ['masthead-hidden'],
+      attributes: true,
+    });
+    return () => {
+      mutationObserver.disconnect();
+    };
+  }, [isFullscreen]);
+
+  return isLive && isTop;
 };
