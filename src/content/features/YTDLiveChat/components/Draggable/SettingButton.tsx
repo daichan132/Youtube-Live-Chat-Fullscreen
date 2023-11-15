e7bf888d7e5db6266de627960236b387632d01e8
daichan132 <wakadai0319@gmail.com>
2023-11-15 11:39:42 +0900

    create Setting Button

--- src/content/features/YTDLiveChat/components/Draggable/SettingButton.tsx
+++ src/content/features/YTDLiveChat/components/Draggable/SettingButton.tsx
@@ -1,15 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { CiSettings } from 'react-icons/ci';
 import styles from '../../styles/Draggable/SettingButton.module.scss';
 import fade from '../../styles/Draggable/Fade.module.scss';
 import { CSSTransition } from 'react-transition-group';
-import { ColorResult, SketchPicker } from 'react-color';
+import { ColorResult, ChromePicker } from 'react-color';
+
+const decimalToHex = (alpha: number) => (alpha === 0 ? '00' : Math.round(255 * alpha).toString(16));
+const propertyList: string[] = [
+  '--yt-live-chat-background-color',
+  '--yt-live-chat-header-background-color',
+  '--yt-live-chat-action-panel-background-color',
+];
 
 export const SettingButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [textColor, setTextColor] = useState<string>('#FFFFFF');
+  const ref = useRef<HTMLIFrameElement | null>(null);
+  useEffect(() => {
+    const element = document.querySelector('#my-extension-root iframe.ytd-live-chat-frame');
+    if (element instanceof HTMLIFrameElement) {
+      ref.current = element;
+    }
+  }, []);
+  const changeIframeBackgroundColor = (color: string) => {
+    if (ref.current && ref.current.contentWindow) {
+      const document = ref.current.contentWindow.document.documentElement;
+      propertyList.forEach((property) => {
+        document.style.setProperty(property, color);
+      });
+    }
+  };
   const changeTextColor = (color: ColorResult) => {
-    setTextColor(color.hex);
+    const hexCode = `${color.hex}${decimalToHex(color.rgb.a || 0)}`;
+    changeIframeBackgroundColor(hexCode);
+    setTextColor(hexCode);
   };
   return (
     <div className={styles['dropDown']}>
@@ -21,7 +45,7 @@ export const SettingButton = () => {
       />
       <CSSTransition in={isOpen} timeout={200} classNames={fade} unmountOnExit>
         <div className={styles['dropDownContent']}>
-          <SketchPicker color={textColor} onChange={changeTextColor} />
+          <ChromePicker color={textColor} onChange={changeTextColor} />
         </div>
       </CSSTransition>
     </div>
