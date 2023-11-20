ffb65ca95a5f4dfcbea3956c20b0d1c65f99c490
daichan132 <wakadai0319@gmail.com>
2023-11-20 15:51:35 +0900

    handle window resize

--- src/content/features/YTDLiveChat/components/Draggable/Draggable.tsx
+++ src/content/features/YTDLiveChat/components/Draggable/Draggable.tsx
@@ -1,9 +1,6 @@
 import { DndContext } from '@dnd-kit/core';
-import React, { useRef, useState } from 'react';
-
-import { Coordinates } from '@dnd-kit/core/dist/types';
+import React from 'react';
 import { restrictToWindowEdges } from '@dnd-kit/modifiers';
-
 import styles from '../../styles/Draggable/Draggable.module.scss';
 import { DraggableItem } from './DraggableItem';
 import { useYTDLiveChatStore } from '../../../../../stores';
@@ -14,22 +11,23 @@ interface DraggableType {
 }
 
 export const Draggable = ({ children }: DraggableType) => {
-  const coordinatesRef = useRef(useYTDLiveChatStore.getState().coordinates);
-  const [{ x, y }, setCoordinates] = useState<Coordinates>(coordinatesRef.current);
-  const { setCoordinates: setCoordinatesToStore } = useYTDLiveChatStore(
-    useShallow((state) => ({ setCoordinates: state.setCoordinates })),
+  const {
+    coordinates: { x, y },
+    setCoordinates,
+  } = useYTDLiveChatStore(
+    useShallow((state) => ({
+      coordinates: state.coordinates,
+      setCoordinates: state.setCoordinates,
+    })),
   );
   return (
     <div className={styles['RestrictWindow']}>
       <DndContext
         onDragEnd={({ delta }) => {
-          setCoordinates(({ x, y }) => {
-            return {
-              x: x + delta.x,
-              y: y + delta.y,
-            };
+          setCoordinates({
+            x: x + delta.x,
+            y: y + delta.y,
           });
-          setCoordinatesToStore({ x: x + delta.x, y: y + delta.y });
         }}
         modifiers={[restrictToWindowEdges]}
       >
