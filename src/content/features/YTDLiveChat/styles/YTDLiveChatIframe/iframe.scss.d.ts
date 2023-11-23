5b8ea85961facf3edc33b01aef7d953b892c6047
daichan132 <wakadai0319@gmail.com>
2023-11-23 15:57:38 +0900

    display scroll bar when user hover

--- src/content/features/YTDLiveChat/styles/YTDLiveChatIframe/iframe.scss.d.ts
+++ src/content/features/YTDLiveChat/styles/YTDLiveChatIframe/iframe.scss.d.ts
@@ -2,11 +2,13 @@ export type Styles = {
   'custom-yt-app-live-chat-extension': string;
   emoji: string;
   item: string;
+  'item-scroller': string;
   message: string;
   'reaction-control-panel': string;
   title: string;
   'yt-dropdown-menu': string;
   'yt-emoji-picker-category-renderer': string;
+  'yt-live-chat-item-list-renderer': string;
   'yt-live-chat-text-message-renderer': string;
   'yt-live-chat-toggle-renderer': string;
   'yt-reaction-control-panel-overlay-view-model': string;
