0e9e2ae46cf2f7d692ee5252b8de6db93307d715
daichan132 <wakadai0319@gmail.com>
2024-01-13 05:49:42 +0900

    fix yarn lint

--- src/popup/components/EmojiCopySwitch.tsx
+++ src/popup/components/EmojiCopySwitch.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
+// import React from 'react';
 
-import { useShallow } from 'zustand/react/shallow';
+// import { useShallow } from 'zustand/react/shallow';
 
-import { Switch } from '../../shared/components/Switch';
-import { useGlobalSettingStore } from '../../stores';
+// import { Switch } from '../../shared/components/Switch';
+// import { useGlobalSettingStore } from '../../stores';
 
-export const EmojiCopySwitch = () => {
-  const { emojiCopy, setEmojiCopy } = useGlobalSettingStore(
-    useShallow((state) => ({
-      emojiCopy: state.emojiCopy,
-      setEmojiCopy: state.setEmojiCopy,
-    })),
-  );
+// export const EmojiCopySwitch = () => {
+//   const { emojiCopy, setEmojiCopy } = useGlobalSettingStore(
+//     useShallow((state) => ({
+//       emojiCopy: state.emojiCopy,
+//       setEmojiCopy: state.setEmojiCopy,
+//     })),
+//   );
 
-  return (
-    <div
-      style={{
-        width: '50px',
-        display: 'flex',
-        justifyContent: 'center',
-      }}
-    >
-      <Switch
-        checked={emojiCopy}
-        id="emoji-copy-switch"
-        onChange={(checked) => {
-          setEmojiCopy(checked);
-          chrome.runtime.sendMessage({ message: 'emojiCopy', emojiCopy: checked });
-        }}
-      />
-    </div>
-  );
-};
+//   return (
+//     <div
+//       style={{
+//         width: '50px',
+//         display: 'flex',
+//         justifyContent: 'center',
+//       }}
+//     >
+//       <Switch
+//         checked={emojiCopy}
+//         id="emoji-copy-switch"
+//         onChange={(checked) => {
+//           setEmojiCopy(checked);
+//           chrome.runtime.sendMessage({ message: 'emojiCopy', emojiCopy: checked });
+//         }}
+//       />
+//     </div>
+//   );
+// };
