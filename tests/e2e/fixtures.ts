558841f4fcbac5781c62de186db17a5babb66d9a
daichan132 <wakadai0319@gmail.com>
2024-01-21 01:09:15 +0900

    fix nano-staged

--- tests/e2e/fixtures.ts
+++ tests/e2e/fixtures.ts
@@ -11,7 +11,7 @@ export const test = base.extend<{
 }>({
   // eslint-disable-next-line no-empty-pattern
   context: async ({}, use) => {
-    const pathToExtension = path.join(__dirname, '../dist');
+    const pathToExtension = path.join(__dirname, '../../dist');
     const context = await chromium.launchPersistentContext('', {
       headless: false,
       args: [
