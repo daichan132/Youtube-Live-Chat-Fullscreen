fc15e8d8e515a92a07dd61cf98c1d068ab5cf13a
daichan132 <wakadai0319@gmail.com>
2024-11-18 20:12:27 +0900

    test: update live chat replay test to check visibility of chat messages without timeout

--- tests/e2e/liveChatReplay.spec.ts
+++ tests/e2e/liveChatReplay.spec.ts
@@ -2,12 +2,10 @@ import { expect, test } from './fixtures'
 
 test('youtube live archive test', async ({ page }) => {
   await page.goto('https://www.youtube.com/watch?v=SBGcCLGkVMo')
-  await page.waitForTimeout(5000)
+  await page.locator('#chatframe').contentFrame().locator('yt-live-chat-text-message-renderer').isVisible
   await page.click('button.ytp-fullscreen-button')
 
   await expect(
-    page.locator(
-      '#extension-root-d774ba85-ed7c-42a2-bf6f-a74e8d8605ec #live-chat-iframe-wrapper iframe#chatframe',
-    ),
+    page.locator('#extension-root-d774ba85-ed7c-42a2-bf6f-a74e8d8605ec #live-chat-iframe-wrapper iframe#chatframe'),
   ).toBeVisible()
 })
