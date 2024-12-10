a3b413add9232bac271c3b35804f426a48c55079
daichan132 <wakadai0319@gmail.com>
2024-12-11 04:09:48 +0900

    yanr lint

--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -3,24 +3,24 @@ import { createPortal } from 'react-dom'
 import { YTDLiveChat } from './YTDLiveChat'
 import { useIsFullScreen } from './hooks/useIsFullscreen'
 
-
 export const Content = () => {
   const { ytdLiveChat } = useGlobalSetting()
   const isFullscreen = useIsFullScreen()
 
   return (
     <>
-      {ytdLiveChat && isFullscreen && (
+      {ytdLiveChat &&
+        isFullscreen &&
         (() => {
-          const portalRoot = document.getElementById('movie_player');
-          if (!portalRoot) return null;
+          const portalRoot = document.getElementById('movie_player')
+          if (!portalRoot) return null
           return createPortal(
             <div className='extension-root-d774ba85-ed7c-42a2-bf6f-a74e8d8605ec '>
               <YTDLiveChat />
-            </div>
-            , portalRoot);
-        })()
-      )}
+            </div>,
+            portalRoot,
+          )
+        })()}
     </>
   )
 }
