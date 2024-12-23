d508739bdaafe10f8bf435b89d031a0a9d858357
daichan132 <wakadai0319@gmail.com>
2024-12-23 14:39:24 +0900

    fix: update live chat test URL and adjust selector for extension root element chore: bump i18next, react-modal, @types/react, @typescript-eslint/parser, lefthook, vite-css-modules versions

--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -18,7 +18,7 @@ export const Content = () => {
           const portalRoot = document.getElementById('movie_player')
           if (!portalRoot) return null
           return createPortal(
-            <div className='extension-root-d774ba85-ed7c-42a2-bf6f-a74e8d8605ec'>
+            <div className='extension-root-d774ba85-ed7c-42a2-bf6f-a74e8d8605ec' id='extension-root-d774ba85-ed7c-42a2-bf6f-a74e8d8605ec'>
               <YTDLiveChat />
             </div>,
             portalRoot,
