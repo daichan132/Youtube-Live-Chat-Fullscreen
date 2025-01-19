574221061ee996aea4d6802cfb86c1fd4307ed8c
daichan132 <wakadai0319@gmail.com>
2025-01-20 06:07:30 +0900

    chore: update CI workflow for extension submission, streamline zipping process, and bump version to 2.1.5

--- wxt.config.ts
+++ wxt.config.ts
@@ -8,9 +8,6 @@ export default defineConfig({
     description: '__MSG_extensionDescription__',
     name: '__MSG_extensionName__',
     default_locale: 'en',
-    browser_specific_settings: {
-      gecko: { id: '{6fecd3d1-1743-4913-af18-f30d06d1fad6}' },
-    },
     permissions: ['activeTab', 'storage'],
   },
 })
